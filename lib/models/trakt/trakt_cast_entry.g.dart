77ce5e0700c4282ee0023ccee82545089dd0fc71
Shasoosh <shasoosh@gmail.com>
2026-07-22 14:39:44 +0000

    style: fix dart format violations

--- lib/models/trakt/trakt_cast_entry.g.dart
+++ lib/models/trakt/trakt_cast_entry.g.dart
@@ -7,25 +7,14 @@ part of 'trakt_cast_entry.dart';
 // **************************************************************************
 
 TraktPersonImages _$TraktPersonImagesFromJson(Map<String, dynamic> json) =>
-    TraktPersonImages(
-      headshot: (json['headshot'] as List<dynamic>?)
-          ?.map((e) => e as String)
-          .toList(),
-    );
+    TraktPersonImages(headshot: (json['headshot'] as List<dynamic>?)?.map((e) => e as String).toList());
 
 TraktPerson _$TraktPersonFromJson(Map<String, dynamic> json) => TraktPerson(
   name: json['name'] as String?,
-  images: json['images'] == null
-      ? null
-      : TraktPersonImages.fromJson(json['images'] as Map<String, dynamic>),
+  images: json['images'] == null ? null : TraktPersonImages.fromJson(json['images'] as Map<String, dynamic>),
 );
 
-TraktCastEntry _$TraktCastEntryFromJson(Map<String, dynamic> json) =>
-    TraktCastEntry(
-      characters: (json['characters'] as List<dynamic>?)
-          ?.map((e) => e as String)
-          .toList(),
-      person: json['person'] == null
-          ? null
-          : TraktPerson.fromJson(json['person'] as Map<String, dynamic>),
-    );
+TraktCastEntry _$TraktCastEntryFromJson(Map<String, dynamic> json) => TraktCastEntry(
+  characters: (json['characters'] as List<dynamic>?)?.map((e) => e as String).toList(),
+  person: json['person'] == null ? null : TraktPerson.fromJson(json['person'] as Map<String, dynamic>),
+);
